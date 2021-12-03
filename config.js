d17ae730733a6cfbe514558285f294bd470c3a41
lacphanminh <95473206+lacphanminh@users.noreply.github.com>
2021-12-03 17:34:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+const  CONFIG  = {
+titleWeb : "giử cho..." ,
+introTitle : 'u.nhi' ,
+introDesc : Thường lệ vốn trái đất
+Mà sao em cứ đi nhầm đường
+Lạc vào tim tớ lẻ loi
+Đằng sau chữ yêu đây là thương` ,
+btnIntro : '^^ HiHi ^^' ,
+title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 🥰' ,
+desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
+btn Có : 'Thích lắm <33' ,
+btnNo : 'Không nha: 3' ,
+question : 'Trên thế giới hơn 7 tỉ lệ người sao cậu lại yêu mình <3' ,
+btnReply : 'Gửi cho mình <3' ,
+reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
+mess : 'tớ biết mà 🥰. Yêu cậu nhiều 😘😘 ' ,                                                              messDesc : 'Tối nay 7h, mình qua đón đi chơi nha. " ,
+btn Chấp nhận : 'Okiiiii lun <3' ,                                                                      messLink : 'https://github.com/zukahai/Confess-Crush'  // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodemy
+}const  CONFIG  =   chữ  yêu  đây  là  thương `🥰. Yêu bạn nhiều 😘😘 ',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
